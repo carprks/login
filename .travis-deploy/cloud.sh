8141486a26c7f7cb79fe99aef0fb3b267952b44b
Keloran <keloran@nordicarts.net>
2019-07-08 21:01:09 +0100

    see if this works

--- .travis-deploy/cloud.sh
+++ .travis-deploy/cloud.sh
@@ -5,7 +5,7 @@ cloudFormationInProgress()
 {
     STACK_NAME=$SERVICE_NAME-$DEPLOY_ENV
 
-    STACK_ROLLBACK=$(AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" aws cloudformation list-stacks --region "$AWS_REGION" --stack-status-filter DELETE_IN_PROGRESS ROLLBACK_IN_PROGRESS CREATE_IN_PROGRESS | jq '.StackSummaries[].StackName//empty' | grep "$STACK_NAME")
+    STACK_ROLLBACK=$(AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" aws cloudformation list-stacks --region "$AWS_REGION" --stack-status-filter DELETE_IN_PROGRESS ROLLBACK_IN_PROGRESS CREATE_IN_PROGRESS UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_IN_PROGRESS UPDATE_COMPLETE_CLEANUP_IN_PROGRESS UPDATE_ROLLBACK_IN_PROGRESS | jq '.StackSummaries[].StackName//empty' | grep "$STACK_NAME")
     if [[ -z "$STACK_ROLLBACK" ]] || [[ "$STACK_ROLLBACK" == "" ]]; then
         echo ""$STACK_NAME" not in progress"
     else
@@ -19,7 +19,7 @@ cloudFormationDelete()
 {
     STACK_NAME=$SERVICE_NAME-$DEPLOY_ENV
 
-    STACK_ROLLBACK=$(AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" aws cloudformation list-stacks --region "$AWS_REGION" --stack-status-filter ROLLBACK_COMPLETE  | jq '.StackSummaries[].StackName//empty' | grep "$STACK_NAME")
+    STACK_ROLLBACK=$(AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" aws cloudformation list-stacks --region "$AWS_REGION" --stack-status-filter ROLLBACK_COMPLETE | jq '.StackSummaries[].StackName//empty' | grep "$STACK_NAME")
     if [[ -z "$STACK_ROLLBACK" ]] || [[ "$STACK_ROLLBACK" == "" ]]; then
         echo ""$STACK_NAME" in good state"
     else
@@ -27,7 +27,7 @@ cloudFormationDelete()
         AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws cloudformation delete-stack \
             --region "$AWS_REGION" \
             --stack-name "$STACK_NAME"
-        sleep 30
+        cloudFormationInProgress
         echo "Deleted Stack "$STACK_NAME""
     fi
 }
@@ -76,7 +76,7 @@ deployIt()
     S3_FOLDER=$S3_BUCKET-$DEPLOY_ENV
 
     AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp cf.yaml s3://"$S3_FOLDER"/$SERVICE_NAME/cf.yaml
-    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp "$TRAVIS_BUILD_ID".zip s3://$S3_FOLDER/$SERVICE_NAME/"$TRAVIS_BUILD_ID".zip
+    #AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp "$TRAVIS_BUILD_ID".zip s3://$S3_FOLDER/$SERVICE_NAME/"$TRAVIS_BUILD_ID".zip
 }
 
 if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
@@ -85,6 +85,13 @@ if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; t
     DNS_ZONE_NAME=$DEV_DNS_ZONE_NAME
     CERTIFICATE_ARN=$DEV_CERTIFICATE_ARN
     AUTHORIZER_ARN=$DEV_AUTHORIZER_ARN
+    AWS_ECR=$DEV_AWS_ECR
+    AWS_VPCID=$DEV_AWS_VPCID
+    AWS_LOADBALANCER_ADDRESS=$DEV_AWS_LOADBALANCER_ADDRESS
+    AWS_LOADBALANCER_LISTENER_ARN=$DEV_AWS_LOADBALANCER_LISTERNER_ARN
+    AWS_SUBNET_ONE=$DEV_AWS_SUBNET_ONE
+    AWS_SUBNET_TWO=$DEV_AWS_SUBNET_TWO
+    AWS_SUBNET_THREE=$DEV_AWS_SUBNET_THREE
 
     echo "Deploy Dev"
     deployIt
@@ -102,6 +109,10 @@ if [[ -z "$TRAVIS_PULL_REQUEST" ]] || [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; t
             DNS_ZONE_NAME=$LIVE_DNS_ZONE_NAME
             CERTIFICATE_ARN=$LIVE_CERTIFICATE_ARN
             AUTHORIZER_ARN=$LIVE_AUTHORIZER_ARN
+            AWS_ECR=$LIVE_AWS_ECR
+            AWS_VPCID=$LIVE_AWS_VPCID
+            AWS_LOADBALANCER_ADDRESS=$LIVE_AWS_LOADBALANCER_ADDRESS
+            AWS_LOADBALANCER_LISTENER_ARN=$LIVE_AWS_LOADBALANCER_LISTERNER_ARN
 
             echo "Deploy Live"
             deployIt
