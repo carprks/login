f1a14cf1b95535b1af52147d3d9ad68eb4a79cad
Keloran <keloran@nordicarts.net>
2019-07-23 01:48:58 +0100

    see if this returns useful info

--- service/login.go
+++ service/login.go
@@ -1,5 +1,33 @@
 package service
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 func LoginService(body string) (string, error) {
+	r := Login{}
+
+	err := json.Unmarshal([]byte(body), &r)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("login unmarshall: %v", err))
+		return "", err
+	}
+
+	fmt.Println("Create Crypt")
+	crypt, err := HashPassword(r.Password)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("crypt password: %v", err))
+		return "", err
+	}
+	r.Crypt = crypt
+	fmt.Println("Created Crypt")
+
+	valid := CheckPassword(crypt, r.Password)
+	if valid {
+		return "success", nil
+	}
+
+
 	return "", nil
 }
